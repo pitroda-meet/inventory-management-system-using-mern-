a676d1b2110afb219d6134a12738d976461ff23b
pitroda-meet <mpitroda459@rku.ac.in>
2025-04-08 10:58:46 +0530

    feat:delete controller for category

--- backend/src/Controllers/CategaryController.js
+++ backend/src/Controllers/CategaryController.js
@@ -26,3 +26,16 @@ export const getCategory = expressAsyncHandler(async (req, res) => {
     res.status(500).json({ message: "Failed to get categories" });
   }
 });
+
+export const deleteCategory = expressAsyncHandler(async (req, res) => {
+  const id = req.params.id;
+  try {
+    const category = await Category.findByIdAndDelete(id);
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
