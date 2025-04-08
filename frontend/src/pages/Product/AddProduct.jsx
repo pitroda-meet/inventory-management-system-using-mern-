e76e2e1bf0eb6135b2b2060f44cee3e3104d37bc
pitroda-meet <mpitroda459@rku.ac.in>
2025-04-08 11:18:43 +0530

    Refactor label for  submit button

--- frontend/src/pages/Product/AddProduct.jsx
+++ frontend/src/pages/Product/AddProduct.jsx
@@ -293,7 +293,7 @@ const AddProduct = () => {
                       type="submit"
                       className="min-w-0 w-full bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
                     >
-                      Update Product
+                      add to Product
                     </button>
                   </form>
                 </div>
